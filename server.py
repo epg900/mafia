2a4b85742cdefa716399312c54dcdffb674c7591
ebrahim pishvaz <e.pishvaz@giloilint.net>
2025-06-07 11:56:51 +0330

    1.21

--- server.py
+++ server.py
@@ -16,7 +16,8 @@ def index():
     return render_template('index.html', path = abs_path , all_list = lst , var1 = 1  )
     
 @app.route('/<path>')
-def run_game(path):
+@app.route('/<path>/<sit>')
+def run_game(path,sit=None):
     if not os.path.exists(f'{abs_path}/{path}'):
         return redirect('/')
     if not os.path.isdir(f'{abs_path}/{path}'):
@@ -26,33 +27,36 @@ def run_game(path):
     lst.sort()
     lst=[l.split("/")[-1] for l in lst]
     lst=[l.split("\\")[-1] for l in lst]
+    random.shuffle(lst)
     
-    e='14'
      		
     file=open(f"{abs_path}/role.txt","r",encoding="utf-8")
     lst2=file.readlines()
     file.close()
+    lst2=[l.strip() for l in lst2]
     lst2=[l.split("\n")[0] for l in lst2]
      		
-    random.shuffle(lst)
     
     lst3=[]
     for l in lst2:
-            if l in lst:
-                    lst3.append(l)
+        if l in lst:
+            lst3.append(l)
     random.shuffle(lst3)
     
-    for i,v in enumerate(e):
-            if int(e[i]):
-                    ix1=lst.index(lst3[i])
-                    iv1=lst[int(e[i])-1]
-                    lst[int(e[i])-1]=lst3[i]
-                    lst[ix1]=iv1   
+    if sit:
+        e=sit                       
+        for i,v in enumerate(e):
+                if int(e[i]):
+                    if len(lst) >= int(e[i]):
+                        ix1=lst.index(lst3[i])
+                        iv1=lst[int(e[i])-1]
+                        lst[int(e[i])-1]=lst3[i]
+                        lst[ix1]=iv1   
     
     length=len(lst)
 
     #return f"senario is {path} <br><br>  lst :  {lst} <br><br> lst2 :  {lst2} <br><br> lst3 :  {lst3} <br><br> {length} "
-    return render_template('index.html', path = path , abs_path = abs_path , all_list = lst , length = length , var1 = 2 )
+    return render_template('index.html', path = path , abs_path = abs_path , all_list = lst , length = length , var1 = 2 , lst2 = lst2 , lst3 = lst3)
         
     
 
